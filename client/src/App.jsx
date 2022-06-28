94669c5066d0a9ef94b8761be7c66970b8d5a9ba
Badre-Edin <dridibadr@gmail.com>
2022-06-28 23:45:42 +0100

    add component MyReservation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import HomePage from '../src/component/HomePage.jsx';
 import AboutUs from '../src/component/AboutUs.jsx'
 import Reservation from '../src/component/Reservation.jsx';
+import MyReservation from './component/MyReservation.jsx';
 
 
 
 function App() {
    const [showHome, setShowHome] = useState(true);
    const [showReservation, setShowReservation] = useState(false);
+   const [showMyReservation, setShowMyReservation] = useState(false);
    const [showAboutUs, setShowAboutUs] = useState(false);
 
 
@@ -16,16 +18,25 @@ function App() {
    const onHomeClick = function () {
       setShowHome(showHome => true)
       setShowReservation(showReservation => false)
+      setShowMyReservation(showMyReservation => false)
       setShowAboutUs(showAboutUs => false)
    }
    const onReservationClick = function () {
       setShowHome(showHome => false)
       setShowReservation(showReservation => true)
+      setShowMyReservation(showMyReservation => false)
+      setShowAboutUs(showAboutUs => false)
+   }
+   const onMyReservationClick = function () {
+      setShowHome(showHome => false)
+      setShowReservation(showReservation => false)
+      setShowMyReservation(showMyReservation => true)
       setShowAboutUs(showAboutUs => false)
    }
    const onAboutUsClick = function () {
       setShowHome(showHome => false)
       setShowReservation(showReservation => false)
+      setShowMyReservation(showMyReservation => false)
       setShowAboutUs(showAboutUs => true)
    }
 
@@ -34,10 +45,12 @@ function App() {
          <div>
             <div onClick={onHomeClick}>Home</div>
             <div onClick={onReservationClick}>Reservation</div>
+            <div onClick={onMyReservationClick}>My Reservation</div>
             <div onClick={onAboutUsClick}>AboutUs</div>
          </div>
          {showHome && <HomePage />}
          {showReservation && <Reservation />}
+         {showMyReservation && <MyReservation />}
          {showAboutUs && <AboutUs />}
 
       </div>
---
+++ client/src/component/MyReservation.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function MyReservation(){
+    const [getReservation,setGetReservation]=useState({});
+    return (
+        <div>
+            <h3>My Reservation</h3>
+        </div>
+    )
+}
+
+
+export default MyReservation;
